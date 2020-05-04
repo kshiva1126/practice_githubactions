99c548c497f93e0288e25a8db6c2c5b59ff0b94a
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2020-05-04 23:26:14 +0900

    add fail test

---
+++ src/tests/Feature/FailTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FailTest extends TestCase
+{
+    /*
+     * @test
+     */
+    public function 失敗()
+    {
+        $this->assertTrue(false);
+    }
+}
